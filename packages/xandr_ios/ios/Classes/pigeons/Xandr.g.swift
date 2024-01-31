3937681a338834714939b551d6f5ee7332faccbf
Markus Korn <markus.korn@gmail.com>
2024-01-31 07:31:14 +0100

    style: add .swiftformat file with formatting rules for Swift code
    🔧 refactor(melos.yaml): remove swiftversion flag from swiftformat command in format script, as it's now defined in .swiftformat file
    🎨 style(XandrPlugin.swift): reformat code to match new rules defined in .swiftformat file

--- packages/xandr_ios/ios/Classes/pigeons/Xandr.g.swift
+++ packages/xandr_ios/ios/Classes/pigeons/Xandr.g.swift
@@ -4,67 +4,70 @@
 import Foundation
 
 #if os(iOS)
-    import Flutter
+  import Flutter
 #elseif os(macOS)
-    import FlutterMacOS
+  import FlutterMacOS
 #else
-    #error("Unsupported platform.")
+  #error("Unsupported platform.")
 #endif
 
 private func wrapResult(_ result: Any?) -> [Any?] {
-    return [result]
+  [result]
 }
 
 private func wrapError(_ error: Any) -> [Any?] {
-    if let flutterError = error as? FlutterError {
-        return [
-            flutterError.code,
-            flutterError.message,
-            flutterError.details,
-        ]
-    }
+  if let flutterError = error as? FlutterError {
     return [
-        "\(error)",
-        "\(type(of: error))",
-        "Stacktrace: \(Thread.callStackSymbols)",
+      flutterError.code,
+      flutterError.message,
+      flutterError.details,
     ]
+  }
+  return [
+    "\(error)",
+    "\(type(of: error))",
+    "Stacktrace: \(Thread.callStackSymbols)",
+  ]
 }
 
 private func isNullish(_ value: Any?) -> Bool {
-    return value is NSNull || value == nil
+  value is NSNull || value == nil
 }
 
 private func nilOrValue<T>(_ value: Any?) -> T? {
-    if value is NSNull { return nil }
-    return value as! T?
+  if value is NSNull { return nil }
+  return value as! T?
 }
 
 /// Generated protocol from Pigeon that represents a handler of messages from Flutter.
 protocol XandrHostApi {
-    func init(memberId: Int64, completion: @escaping (Result<Bool, Error>) -> Void)
+  func init(memberId: Int64, completion: @escaping (Result<Bool, Error>) -> Void)
 }
 
 /// Generated setup class from Pigeon to handle messages through the `binaryMessenger`.
 enum XandrHostApiSetup {
-    /// The codec used by XandrHostApi.
-    /// Sets up an instance of `XandrHostApi` to handle messages through the `binaryMessenger`.
-    static func setUp(binaryMessenger: FlutterBinaryMessenger, api: XandrHostApi?) {
-        let initChannel = FlutterBasicMessageChannel(name: "dev.flutter.pigeon.xandr_ios.XandrHostApi.init", binaryMessenger: binaryMessenger)
-        if let api = api {
-            initChannel.setMessageHandler { message, reply in
-                let args = message as! [Any?]
-                let memberIdArg = args[0] is Int64 ? args[0] as! Int64 : Int64(args[0] as! Int32)
-                api.init(memberId: memberIdArg) { result in
-                    switch result {
-                    case let .success(res):
-                        reply(wrapResult(res))
-                    case let .failure(error):
-                        reply(wrapError(error))
-                    }
-                }
-            }
-        } else {
-            initChannel.setMessageHandler(nil)
+  /// The codec used by XandrHostApi.
+  /// Sets up an instance of `XandrHostApi` to handle messages through the `binaryMessenger`.
+  static func setUp(binaryMessenger: FlutterBinaryMessenger, api: XandrHostApi?) {
+    let initChannel = FlutterBasicMessageChannel(
+      name: "dev.flutter.pigeon.xandr_ios.XandrHostApi.init",
+      binaryMessenger: binaryMessenger
+    )
+    if let api {
+      initChannel.setMessageHandler { message, reply in
+        let args = message as! [Any?]
+        let memberIdArg = args[0] is Int64 ? args[0] as! Int64 : Int64(args[0] as! Int32)
+        api.init(memberId: memberIdArg) { result in
+          switch result {
+          case let .success(res):
+            reply(wrapResult(res))
+          case let .failure(error):
+            reply(wrapError(error))
+          }
         }
+      }
+    } else {
+      initChannel.setMessageHandler(nil)
     }
+  }
 }
