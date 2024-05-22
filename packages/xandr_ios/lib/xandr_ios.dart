d5fb67d633260e246e9551007726a57502de87c4
ashcherbonos <30783567+ashcherbonos@users.noreply.github.com>
2024-05-22 13:22:04 +0200

    XandrIOS bridge

--- packages/xandr_ios/lib/xandr_ios.dart
+++ packages/xandr_ios/lib/xandr_ios.dart
@@ -1,11 +1,55 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:collection/collection.dart';
 
 import 'package:flutter/foundation.dart';
 import 'package:xandr_ios/src/messages.g.dart' as messages;
 import 'package:xandr_platform_interface/xandr_platform_interface.dart';
 
-/// The Android implementation of [XandrPlatform].
+extension on messages.HostAPIUserId {
+  UserId toUserId() {
+    switch (source) {
+      case messages.HostAPIUserIdSource.criteo:
+        return UserId.criteo(userId);
+      case messages.HostAPIUserIdSource.theTradeDesk:
+        return UserId.theTradeDesk(userId);
+      case messages.HostAPIUserIdSource.netId:
+        return UserId.netId(userId);
+      case messages.HostAPIUserIdSource.liveramp:
+        return UserId.liveramp(userId);
+      case messages.HostAPIUserIdSource.uid2:
+        return UserId.uid2(userId);
+    }
+  }
+}
+
+extension on UserIdSource {
+  messages.HostAPIUserIdSource toHostAPIUserIdSource() {
+    switch (this) {
+      case UserIdSource.criteo:
+        return messages.HostAPIUserIdSource.criteo;
+      case UserIdSource.theTradeDesk:
+        return messages.HostAPIUserIdSource.theTradeDesk;
+      case UserIdSource.netId:
+        return messages.HostAPIUserIdSource.netId;
+      case UserIdSource.liveramp:
+        return messages.HostAPIUserIdSource.liveramp;
+      case UserIdSource.uid2:
+        return messages.HostAPIUserIdSource.uid2;
+    }
+  }
+}
+
+extension on UserId {
+  messages.HostAPIUserId toHostAPIUserId() {
+    return messages.HostAPIUserId(
+      userId: userId,
+      source: source.toHostAPIUserIdSource(),
+    );
+  }
+}
+
+/// The iOS implementation of [XandrPlatform].
 class XandrIOS extends XandrPlatform {
   final messages.XandrHostApi _api = messages.XandrHostApi();
 
@@ -49,6 +93,59 @@ class XandrIOS extends XandrPlatform {
     return _api.showInterstitialAd(
       autoDismissDelay: autoDismissDelay?.inSeconds,
     );
+  }
+
+  @override
+  Future<void> setPublisherUserId(String publisherUserId) {
+    return _api.setPublisherUserId(publisherUserId: publisherUserId);
+  }
+
+  @override
+  Future<String> getPublisherUserId() {
+    return _api.getPublisherUserId();
+  }
+
+  @override
+  Future<void> setUserIds(List<UserId> userIds) {
+    return _api.setUserIds(
+      userIds.map((uId) => uId.toHostAPIUserId()).toList(),
+    );
+  }
+
+  @override
+  Future<List<UserId>> getUserIds() async {
+    final userIds = await _api.getUserIds();
+    return userIds.whereNotNull().map((uId) => uId.toUserId()).toList();
+  }
+
+  @override
+  Future<String> initMultiAdRequest() {
+    return _api.initMultiAdRequest();
+  }
+
+  @override
+  Future<void> disposeMultiAdRequest(String multiAdRequestID) {
+    return _api.disposeMultiAdRequest(multiAdRequestID);
+  }
+
+  @override
+  Future<bool> loadAdsForMultiAdRequest(String multiAdRequestID) {
+    return _api.loadAdsForMultiAdRequest(multiAdRequestID);
+  }
+
+  @override
+  Future<void> setGDPRConsentRequired(bool isConsentRequired) {
+    return _api.setGDPRConsentRequired(isConsentRequired: isConsentRequired);
+  }
+
+  @override
+  Future<void> setGDPRConsentString(String consentString) {
+    return _api.setGDPRConsentString(consentString: consentString);
+  }
+
+  @override
+  Future<void> setGDPRPurposeConsents(String purposeConsents) {
+    return _api.setGDPRPurposeConsents(purposeConsents: purposeConsents);
   }
 }
 
